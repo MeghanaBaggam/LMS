cc6179df52ca0a8f469c6af8d7c2dfc5b2f5bcdf
Meghana <meghanabaggam@gmail.com>
2025-11-10 17:08:19 +0530

    Updated routes with middleware authentication

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LeaveController;
 use App\Http\Controllers\AuthController;
 
-Route::post('leaves/{leave}/approve',[LeaveController::class, 'approve']);
-Route::post('leaves/{leave}/reject',[LeaveController::class, 'reject']);
 
+Route::post('login',[AuthController::class,'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('user-data', [AuthController::class, 'userData']);
+    Route::apiResource('leaves', LeaveController::class);
+    Route::post('leaves/{leave}/approve',[LeaveController::class, 'approve']);
+    Route::post('leaves/{leave}/reject',[LeaveController::class, 'reject']);
+});
