1ab4dfa0057e48aa3d51fb69897fee0476fd4886
Meghana <meghanabaggam@gmail.com>
2025-11-10 16:19:16 +0530

    Created Leave Model

---
+++ app/Models/Leave.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Leave extends Model
+
+{
+    protected $fillable=[
+        'user_id','start_date','end_date','type','reason','status','approved_by',
+        'rejected_reason','days'
+    ];
+    //get the user who requested the leave
+    public function user(): BelongsTo
+    {
+        
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    //get the manager/hr who approves the leave
+    public function approver(): BelongsTo
+    {
+        
+        return $this->belongsTo(User::class, 'approved_by');
+    }
+}
+
