3484416259bc733653bca6967e1f44e0dcfcd0b2
Meghana <meghanabaggam@gmail.com>
2025-11-11 10:44:28 +0530

    Added crud operations for HR by creating the route for EmployeeController

--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LeaveController;
 use App\Http\Controllers\AuthController;
 
-
+//public
 Route::post('login', [AuthController::class, 'login']);
-
+//protected
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('user-data', [AuthController::class, 'userData']);
     Route::apiResource('leaves', LeaveController::class);
     Route::post('leaves/{leave}/approve',[LeaveController::class, 'approve']);
     Route::post('leaves/{leave}/reject',[LeaveController::class, 'reject']);
+});
+//restricted
+Route::middleware(['auth:sanctum','can:isHR'])->group(function (){
+    Route::apiResource('employees',EmployeeController::class);
 });
