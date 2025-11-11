6895e9bf06b3fd1ea3ddeebd4b75c441bede3a70
Meghana <meghanabaggam@gmail.com>
2025-11-11 11:18:00 +0530

    Created a Seeder to add sample HR, Manager, Employee users(dummy data)

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        $hr=User::create([
+            'name'=>'Shyampal Reddy',
+            'email'=>'shyampalreddy@pal.tech',
+            'password'=>Hash::make('password'),
+            'role'=>'hr',
+            'leave_balance'=>20
+        ]);
+
+        $manager=User::create([
+             'name'=>'Santhosh',
+            'email'=>'santhosh@pal.tech',
+            'password'=>Hash::make('password'),
+            'role'=>'manager',
+            'leave_balance'=>20
+        ]);
+        $emp=User::create([
+            'name'=>'Meghana Baggam',
+            'email'=>'meghanabaggam@pal.tech',
+            'password'=>Hash::make('password'),
+            'role'=>'employee',
+            'manager_id'=>$manager->id,
+            'leave_balance'=>20
+        ]);
+    }
+}
