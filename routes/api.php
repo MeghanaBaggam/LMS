4955a47e410b6513e3740ee0b84cfe073ec34f9f
Meghana <meghanabaggam@gmail.com>
2025-11-12 18:17:29 +0530

    updated routes

--- routes/api.php
+++ routes/api.php
@@ -16,24 +16,24 @@ Route::middleware('auth:api')->group(function () {
     Route::get('user-data', [AuthController::class, 'userData']);
     
     //employee actions
-    Route::middleware('role:employee')->group(function (){
+    Route::middleware(['role:employee'])->group(function (){
         Route::post('leaves',[LeaveController::class,'store']);
         Route::get('leaves',[LeaveController::class,'index']);
     });
 
     //manager actions
-    Route::middleware('role:manager')->group(function (){
+    Route::middleware(['role:manager'])->group(function (){
         Route::get('team/leaves',[LeaveController::class,'index']);
         Route::post('leaves/{leaves}/approve',[LeaveController::class,'approve']);
         Route::post('leaves/{leaves}/reject',[LeaveController::class,'reject']);
     });
     //HR actions
-    Route::middleware('role:hr')->group(function (){
+    Route::middleware(['role:hr'])->group(function (){
         Route::get('all/leaves',[LeaveController::class,'index']);
         Route::apiResource('employees',EmployeeController::class);
     });
     //admin actions
-    Route::middleware('role:admin')->group(function (){
+    Route::middleware(['role:admin'])->group(function (){
         Route::get('roles',function (){
             return \Spatie\Permission\Models\Role::all();
         });
