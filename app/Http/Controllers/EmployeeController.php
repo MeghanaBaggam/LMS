45a94c6bb58f574701fd05f7a598dc6514719930
Meghana <meghanabaggam@gmail.com>
2025-11-13 10:21:43 +0530

    Updated EmployeeController with RBAC

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -8,7 +8,7 @@ use  Illuminate\Support\Facades\Hash;
 class EmployeeController extends Controller
 {
     public function index(){
-        return User::orderBy('name')->get();
+        return User::with('roles')->orderBy('name')->get();
     }
     public function store(Request $req){
         $req->validate([
@@ -22,15 +22,27 @@ class EmployeeController extends Controller
             'name'=>$req->name,
             'email'=>$req->email,
             'password'=>Hash::make($req->password),
-            'role'=>$req->role,
-            'manager_id'=>auth()->id(),
+            'manager_id'=>$req->manager_id,//hr choose manager
             'leave_balance'=>20,
         ]);
+        $user->assignRole($req->role);
         return response()->json($user,201);
     }
     public function update(Request $req,User $user){
-        $data=$req->only(['name','email','role','manager_id','leave_balance']);
-        $user->update($data);
+        $req->validate([
+            'name'        => 'sometimes',
+            'email'       => 'sometimes|email|unique:users,email,' . $user->id,
+            'role'        => 'sometimes|in:employee,manager,hr',
+            'manager_id'  => 'nullable|exists:users,id',
+            'leave_balance' => 'sometimes|numeric'
+        ]);
+        $data=$req->only(['name','email','manager_id','leave_balance']);
+         $user->update($data);
+        //if role updated
+        if($req->role){
+            $user->syncRoles([$req->role]);
+        }
+       
         return response()->json($user);
     }
     public function destroy(User $user){
