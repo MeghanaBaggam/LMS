9d45f667c8df59cd49cebe0b0dba58ddf76188d8
Meghana <meghanabaggam@gmail.com>
2025-11-20 10:46:49 +0530

    updated all dashboards logout navigation with usenavigate hook

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import { createContext, useState } from 'react'
+import React from "react";
+import { createContext, useState } from "react";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
-    const [token, setToken] = useState(localStorage.getItem("token"));
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
+  const [token, setToken] = useState(localStorage.getItem("token"));
 
-    const login = (useData, tokenValue) => {
-        setUser(useData);
-        setToken(tokenValue);
-        localStorage.setItem("user", JSON.stringify(useData));
-        localStorage.setItem("token", tokenValue);
-    };
-    const logout = () => {
-        setUser("");
-        setToken("");
-        localStorage.clear();
-        window.location.href = "/";
-    };
+  const login = (useData, tokenValue) => {
+    setUser(useData);
+    setToken(tokenValue);
+    localStorage.setItem("user", JSON.stringify(useData));
+    localStorage.setItem("token", tokenValue);
+  };
+  const logout = () => {
+    setUser("");
+    setToken("");
+    localStorage.clear();
+    window.location.href = "/";
+  };
 
-    return (
-        <AuthContext.Provider value={{ user, token, login, logout }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ user, token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
--- src/Pages/EmployeeDashboard.jsx
+++ src/Pages/EmployeeDashboard.jsx
@@ -1,77 +1,84 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { FaUserCircle } from "react-icons/fa";
-import { NavLink, Outlet } from 'react-router-dom';
-
+import { NavLink, Outlet } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export const EmployeeDashboard = () => {
-    const [showMenu, setShowMenu] = useState(false);
-    const [showProfile, setShowProfile] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const [showProfile, setShowProfile] = useState(false);
+  const navigate = useNavigate();
+  const user = JSON.parse(localStorage.getItem("user"));
+  const logoutUser = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    navigate("/");
+  };
+  return (
+    <div className="hr-container">
+      <div className="top-right-menu">
+        <div className="user-info" onClick={() => setShowMenu(!showMenu)}>
+          <div className="avatar-circle">
+            <FaUserCircle />
+          </div>
+          <span className="user-name">{user?.name}</span>
 
-
-    const user = JSON.parse(localStorage.getItem("user"));
-    const logoutUser = () => {
-        localStorage.removeItem("token");
-        localStorage.removeItem("user");
-        window.location.href = "/";
-    };
-    return (
-        <div className="hr-container">
-            <div className="top-right-menu">
-                <div className="user-info" onClick={() => setShowMenu(!showMenu)}>
-                    <div className="avatar-circle">
-                        <FaUserCircle />
-                    </div>
-                    <span className="user-name">{user?.name}</span>
-
-                    {showMenu && (
-                        <div className="dropdown-box">
-                            <p onClick={() => setShowProfile(true)}>Profile</p>
-                            <p className="logout" onClick={logoutUser}>Logout</p>
-                        </div>
-                    )}
-                </div>
+          {showMenu && (
+            <div className="dropdown-box">
+              <p onClick={() => setShowProfile(true)}>Profile</p>
+              <p className="logout" onClick={logoutUser}>
+                Logout
+              </p>
             </div>
-            {showProfile && (
-                <div className="model">
-                    <div className="model-content">
-                        <h3>User Profile</h3>
-                        <p><strong>ID:</strong> {user?.id}</p>
-                        <p><strong>Name:</strong> {user?.name}</p>
-                        <p><strong>Email:</strong> {user?.email}</p>
-                        <p><strong>Role:</strong> {user?.role}</p>
-                        <p><strong>Manager ID:</strong> {user?.manager_id || "-"}</p>
-                        <button onClick={() => setShowProfile(false)}>Close</button>
-                    </div>
-                </div>
-            )}
-
-            <h2 className="welcome-text">Welcome {user?.name}!</h2>
-
-            <div className="top-nav">
-
-                {[
-                    { path: "leave-balance", label: "Leave Balance" },
-                    { path: "leave-requests", label: "Leave Requests" },
-
-                ].map(tab => (
-                    <NavLink
-                        key={tab.path}
-                        to={tab.path}
-                        className={({ isActive }) =>
-                            isActive ? "nav-item active-tab" : "nav-item"
-                        }
-
-                    >
-                        {tab.label}
-                    </NavLink>
-                ))}
-            </div>
-            <div>
-                <Outlet />
-            </div>
-
+          )}
         </div>
-    );
+      </div>
+      {showProfile && (
+        <div className="model">
+          <div className="model-content">
+            <h3>User Profile</h3>
+            <p>
+              <strong>ID:</strong> {user?.id}
+            </p>
+            <p>
+              <strong>Name:</strong> {user?.name}
+            </p>
+            <p>
+              <strong>Email:</strong> {user?.email}
+            </p>
+            <p>
+              <strong>Role:</strong> {user?.role}
+            </p>
+            <p>
+              <strong>Manager ID:</strong> {user?.manager_id || "-"}
+            </p>
+            <button onClick={() => setShowProfile(false)}>Close</button>
+          </div>
+        </div>
+      )}
+
+      <h2 className="welcome-text">Welcome {user?.name}!</h2>
+
+      <div className="top-nav">
+        {[
+          { path: "leave-balance", label: "Leave Balance" },
+          { path: "leave-requests", label: "Leave Requests" },
+        ].map((tab) => (
+          <NavLink
+            key={tab.path}
+            to={tab.path}
+            className={({ isActive }) =>
+              isActive ? "nav-item active-tab" : "nav-item"
+            }
+          >
+            {tab.label}
+          </NavLink>
+        ))}
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default EmployeeDashboard;
