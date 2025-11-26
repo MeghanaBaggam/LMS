14fa5e86e54ab704026871028192ad927f63cafe
Meghana <meghanabaggam@gmail.com>
2025-11-26 10:57:27 +0530

    created a store in store folder

---
+++ src/store/store.jsx
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { employeeReducer } from "./EmployeeSlice";
+
+const store = configureStore({
+    reducer: {
+        employees: employeeReducer
+    }
+});
+export default store;
