9c97d9982c4ffec3e1591ffa40fff670e4df041c
Meghana <meghanabaggam@gmail.com>
2025-11-26 11:01:14 +0530

    wrapped the main app in redux provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,18 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './Context/AuthContext.jsx'
+import { Provider } from 'react-redux'
+import store from './store/store.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
     <AuthProvider>
           <App />
     </AuthProvider> 
     </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
